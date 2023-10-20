c0b0fb0fa14df0bda3be4c2328de812511654f7f
SRCthird <SRCthird@gmail.com>
2023-10-20 18:53:04 -0400

    Add function for cascading dropdown

--- manager/operations/views.py
+++ manager/operations/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
 
 # Create your views here.
+
+def get_part_numbers(request):
+    id_workorder = request.GET.get('id_workorder')
+    if id_workorder:
+        part_numbers = catalog_material.objects.filter(
+            catalog_number__workorder__id=id_workorder
+        ).values_list('id', 'material_name')
+        return JsonResponse(list(part_numbers), safe=False)
+    return JsonResponse([], safe=False)
