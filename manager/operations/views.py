c4660ae35557a2077992a54a36c498ec012422dd
SRCthird <SRCthird@gmail.com>
2023-10-20 19:32:09 -0400

    Corrected view and added documentation

--- manager/operations/views.py
+++ manager/operations/views.py
@@ -5,10 +5,21 @@ from .models import *
 # Create your views here.
 
 def get_part_numbers(request):
-    id_workorder = request.GET.get('id_workorder')
-    if id_workorder:
-        part_numbers = catalog_material.objects.filter(
-            catalog_number__workorder__id=id_workorder
-        ).values_list('id', 'material_name')
+    """
+    This view pulls the finds the all materials that are part of the selected Workorder.
+    @param request: The request work order.
+    @return: A list of part numbers.
+    """
+    workorder_id = request.GET.get('workorder_id')
+    if workorder_id:
+        workorder_instance = workorder.objects.get(pk=workorder_id)
+        catalog_num = workorder_instance.catalog_number
+        
+        part_numbers = catalog_material \
+            .objects \
+            .filter(catalog_number=catalog_num) \
+            .values_list('id', 'material_name')
+        
         return JsonResponse(list(part_numbers), safe=False)
+    
     return JsonResponse([], safe=False)
