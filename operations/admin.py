eab6900c2524ea6bde4ca1fa27550df0e95e293c
Stephen Chryn <SRCthird@gmail.com>
2023-10-22 20:16:03 -0400

    Move django to new directory

---
+++ operations/admin.py
@@ -0,0 +1,210 @@
+from manager import settings
+from django.contrib import admin
+from django.db.models import Sum
+from django.utils.dateformat import DateFormat
+from . import forms
+from .models import *
+
+admin.AdminSite.site_header = settings.OPERATIONS_ADMIN_HEADER
+admin.AdminSite.site_title =  settings.OPERATIONS_ADMIN_TITLE
+admin.AdminSite.index_title = settings.OPERATIONS_ADMIN_INDEX_TITLE
+
+# Register your models here.        
+@admin.register(department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'icon')
+    list_filter = ('name', 'description')
+    search_fields = ('name', 'description')
+
+    fieldsets = (
+        (None, {
+            'fields': ('id', 'name', 'description', 'icon')
+        }),
+    )
+
+@admin.register(employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'department_id', 'role')
+    list_filter = ('user', 'department_id', 'role')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('user',)
+        }),
+        ('Additional Information', {
+            'fields': ('department_id', 'role')
+        }),
+    )
+
+@admin.register(line)
+class LineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'department')
+    list_filter = ('name', 'description', 'department')
+    search_fields = ('name', 'description', 'department__name')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'department')
+        }),
+    )
+
+@admin.register(line_goal)
+class LineGoalAdmin(admin.ModelAdmin):
+    list_display = ('line', 'shift', 'total_good', 'total_downtime', 'percent_reject')
+    list_filter = ('line', 'shift')
+    search_fields = ('line__name','shift')
+
+    fieldsets = (
+        ('Line information', {
+            'fields': ('line','shift') 
+        }),
+        ('Line statistics', {
+            'fields': ('total_good', 'total_downtime', 'percent_reject')
+        }),
+    )
+
+@admin.register(line_reject)
+class LineRejectAdmin(admin.ModelAdmin):
+    list_display = ('line', 'reason')
+    list_filter = ('line','reason')
+    search_fields = ('line__name','reason')
+
+    fieldsets = (
+        ('Line information', {
+            'fields': ('line','reason') 
+        }),
+    )
+
+@admin.register(line_downtime)
+class LineDowntimeAdmin(admin.ModelAdmin):
+    list_display = ('line','reason')
+    list_filter = ('line','reason')
+    search_fields = ('line__name','reason')
+
+    fieldsets = (
+        ('Line information', {
+            'fields': ('line','reason') 
+        }),
+    )
+
+@admin.register(catalog_number)
+class CatalogNumberAdmin(admin.ModelAdmin):
+    list_display = ('catalog_number', 'description')
+    list_filter = ('catalog_number',)
+    search_fields = ('catalog_number', 'description')
+
+    fieldsets = (
+        ('Catalog Number Information', {
+            'fields': ('catalog_number', 'description')
+        }),
+    )
+
+@admin.register(catalog_material)
+class CatalogMaterialAdmin(admin.ModelAdmin):
+    list_display = ('catalog_number', 'material_name', 'avg_per_unit', 'unit_of_measure')
+    list_filter = ('catalog_number', 'material_name')
+    search_fields = ('catalog_material', 'material_name', 'unit_of_measure')
+
+    fieldsets = (
+        ('Catalog Number Information', {
+            'fields': ('catalog_number', 'material_name', 'avg_per_unit', 'unit_of_measure')
+        }),
+    )
+
+@admin.register(workorder)
+class WorkorderAdmin(admin.ModelAdmin):
+    list_display = ('workorder', 'lot_number', 'status', 'catalog_number', 'quantity_start', 'sum_quantity_output', 'priority', 'scheduled_start_dd_mm_yy', 'due_date_dd_mm_yy', 'actual_start_dd_mm_yy', 'wj_printed', 'br_printed', 'labels_ordered', 'labels_received')
+    list_filter = ('workorder', 'lot_number', 'status', 'catalog_number', 'priority', 'scheduled_start', 'due_date')
+    search_fields = ('workorder', 'lot_number', 'status', 'catalog_number', 'priority','scheduled_start', 'due_date')
+
+    ordering = ['status', 'due_date']
+
+    fieldsets = (
+        ('Workorder Information', {
+            'fields': ('workorder', 'lot_number', 'catalog_number'),
+        }),
+        ('Workorder Statistics', {
+            'fields': ('scheduled_start', 'due_date', 'quantity_start', 'priority')
+        }),
+        ('Workorder Preparation', {
+            'fields': ('wj_printed', 'br_printed', 'labels_ordered', 'labels_received')
+        }),
+        ('Workorder Processing', {
+            'fields': ('actual_start', 'status')
+        }),
+    )
+    
+    def scheduled_start_dd_mm_yy(self, obj):
+        if obj.scheduled_start is not None:
+            # Format the date as "dd-mmm-yy"
+            return DateFormat(obj.scheduled_start).format('d-M-y')
+        else:
+            return '-'
+    scheduled_start_dd_mm_yy.admin_order_field ='scheduled_start'
+    scheduled_start_dd_mm_yy.short_description = 'Scheduled Start Date'
+    
+    def due_date_dd_mm_yy(self, obj):
+        if obj.due_date is not None:
+            # Format the date as "dd-mmm-yy"
+            return DateFormat(obj.due_date).format('d-M-y')
+        else:
+            return '-'
+    due_date_dd_mm_yy.admin_order_field = 'due_date'
+    due_date_dd_mm_yy.short_description = 'Scheduled Due Date'
+    
+    def actual_start_dd_mm_yy(self, obj):
+        if obj.actual_start is not None:
+            # Format the date as "dd-mmm-yy"
+            return DateFormat(obj.actual_start).format('d-M-y')
+        else:
+            return '-'
+    actual_start_dd_mm_yy.admin_order_field = 'actual_start'
+    actual_start_dd_mm_yy.short_description = 'Actual Start Date'
+
+    def sum_quantity_output(self, obj):
+        # Calculate the sum of quantity from related operations_output
+        total_quantity = output \
+            .objects \
+            .filter(workorder=obj) \
+            .aggregate(Sum('quantity'))['quantity__sum']
+
+        # Check if total_quantity is None (no matching rows) and return 0 instead
+        return total_quantity if total_quantity is not None else 0
+    sum_quantity_output.short_description = 'Total Quantity'
+
+@admin.register(schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('workorder', 'line')
+    list_filter = ('workorder', 'line')
+    search_fields = ('workorder', 'line')
+
+    fieldsets = (
+        ('Schedule Information', {
+            'fields': ('workorder', 'line')
+        }),
+    )
+
+@admin.register(material)
+class MaterialAdmin(admin.ModelAdmin):
+
+    form = forms.MaterialAdminForm
+    
+    list_display = ('line', 'request_type', 'workorder', 'part_number', 'quantity', 'line_down_at', 'uid', 'comments', 'delivery_status')
+    list_filter = ('line','request_type', 'workorder', 'delivery_status')
+    search_fields = ('line__name','request_type', 'workorder__workorder_number',)
+
+    fieldsets = (
+        ('Line Information', {
+            'fields': ('line', 'workorder', 'line_down_at')
+        }),
+        ('Material Request', {
+            'fields': ('request_type', 'part_number', 'quantity')
+        }),
+        ('Request Information', {
+            'fields': ('uid', 'comments', 'delivery_status')
+        })
+    )
+    
+    class Media:
+        js = ('operations/js/dynamic_updates.js',) # Dynamically update part number based on workorder's catalog number
---
+++ operations/models.py
@@ -0,0 +1,354 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+class department(models.Model):
+    id = models.CharField(
+        max_length=25, 
+        primary_key=True
+    )
+    name = models.CharField(
+        max_length=255
+    )
+    description = models.TextField()
+    icon = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class line(models.Model):
+    name = models.CharField(
+        max_length=255, 
+        primary_key=True,
+        help_text="Enter the name of the line. This field must be unique."
+    )
+    description = models.TextField()
+    department = models.ForeignKey(
+        department, 
+        on_delete=models.CASCADE, 
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+    
+class line_goal(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    line = models.ForeignKey(
+        line, 
+        on_delete=models.CASCADE,
+        to_field='name'
+    )
+    shift = models.CharField(
+        max_length=1
+    )
+    total_good = models.IntegerField()
+    total_downtime = models.IntegerField()
+    percent_reject = models.DecimalField(
+        max_digits=5, 
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return f"{self.line}: {self.shift} Shift"
+    
+    class Meta:
+        verbose_name = "Line Goal"
+        ordering = ['line', 'shift']
+
+class line_reject(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    line = models.ForeignKey(
+        line, 
+        on_delete=models.CASCADE,
+        to_field='name'
+    )
+    reason = models.CharField(
+        max_length=25
+    )
+
+    def __str__(self):
+        return f"{self.line}: {self.reason}"
+    
+    class Meta:
+        verbose_name = "Line Reject"
+
+class line_downtime(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    line = models.ForeignKey(
+        line, 
+        on_delete=models.CASCADE,
+        to_field='name'
+    )
+    reason = models.CharField(
+        max_length=25
+    )
+
+    def __str__(self):
+        return f"{self.line}: {self.reason}"
+    
+    class Meta:
+        verbose_name = "Line Downtime"
+class employee(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE
+    )
+    department_id = models.ForeignKey(
+        department,  
+        on_delete=models.CASCADE,
+        to_field='id'
+    )
+    role = models.CharField(
+        max_length=25
+    )
+    
+    def __str__(self):
+        return self.user.username
+    
+class catalog_number(models.Model):
+    catalog_number = models.CharField(
+        max_length=255,
+        primary_key=True
+    )
+    description = models.CharField(
+        max_length=500
+    )
+
+    def __str__(self):
+        return self.catalog_number
+    
+    class Meta:
+        verbose_name = "Catalog Number"
+
+class catalog_material(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    catalog_number = models.ForeignKey(
+        catalog_number, 
+        on_delete=models.CASCADE,
+        to_field='catalog_number'
+    )
+    material_name = models.CharField(
+        max_length=255
+    )
+    avg_per_unit = models.DecimalField(
+        max_digits=15, decimal_places=6
+    )
+    unit_of_measure = models.CharField(
+        max_length=25
+    )
+
+    def __str__(self):
+        return f"{self.catalog_number}: {self.material_name}"
+    
+    class Meta:
+        verbose_name = "Catalog Material"
+
+class workorder(models.Model):
+    status_choice = [
+        (1, 'Released'),
+        (2, 'In Progress'),
+        (3, 'On Hold'),
+        (4, 'Rework'),
+        (5, 'Lot Release'),
+        (6, 'Scrapped'),
+        (7, 'Completed'),
+    ]
+    workorder = models.CharField(
+        max_length=25, 
+        primary_key=True
+    )
+    lot_number = models.CharField(
+        max_length=25
+    )
+    status = models.CharField(
+        max_length=25,
+        choices=status_choice,
+        default='Scheduled'
+    )
+    catalog_number = models.ForeignKey(
+        catalog_number,  
+        on_delete=models.CASCADE,
+        to_field='catalog_number'
+    )
+    quantity_start = models.IntegerField()
+    priority = models.IntegerField()
+    scheduled_start = models.DateField()
+    actual_start = models.DateField(
+        null=True,
+        blank=True,
+        help_text="The actual start date of the workorder."
+    )
+    due_date = models.DateField()
+    wj_printed = models.BooleanField(
+        default=False
+    )
+    br_printed = models.BooleanField(
+        default=False
+    )
+    labels_ordered = models.BooleanField(
+        default=False
+    )
+    labels_received = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return self.workorder
+    
+class schedule(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    workorder = models.ForeignKey(
+        workorder,
+        on_delete=models.CASCADE,
+        to_field='workorder'
+    )
+    line = models.ForeignKey(
+        line, 
+        on_delete=models.CASCADE,
+        to_field='name'
+    )
+
+    def __str__(self):
+        return f"{self.workorder} on {self.line}"
+
+class material(models.Model):
+    request_choice = [
+        (1, 'New Lot'),
+        (2, 'New Rack'),
+        (3, 'Replacement Rack'),
+        (4, 'Project Lot'),
+    ]
+    
+    def default_time_down():
+        """Get the current time + 90 minutes"""
+        print("Time now: ", timezone.now())
+        return timezone.now() + datetime.timedelta(minutes=90)
+    
+    id = models.AutoField(
+        primary_key=True
+    )
+    line = models.ForeignKey(
+        line, 
+        on_delete=models.CASCADE,
+        to_field='name'
+    )
+    request_type = models.CharField(
+        max_length=25,
+        choices=request_choice
+    )
+    workorder = models.ForeignKey(
+        workorder, 
+        on_delete=models.CASCADE,
+        to_field='workorder'
+    )
+    part_number = models.ForeignKey(
+        catalog_material, 
+        on_delete=models.CASCADE
+    )
+    quantity = models.IntegerField()
+    line_down_at = models.DateTimeField(
+        default=default_time_down
+    )
+    uid = models.ForeignKey(
+        employee, 
+        on_delete=models.CASCADE
+    )
+    comments = models.TextField()
+    delivery_status = models.CharField(
+        max_length=25
+    )
+
+    def __str__(self):
+        return f"{self.workorder} on {self.line}: {self.part_number} {self.quantity}"
+    
+class output(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    employee = models.ForeignKey(
+        employee, 
+        on_delete=models.CASCADE
+    )
+    shift = models.CharField(
+        max_length=1
+    )
+    line = models.ForeignKey(
+        line, 
+        on_delete=models.CASCADE,
+        to_field='name'
+    )
+    date = models.DateField()
+    workorder = models.ForeignKey(
+        workorder,
+        on_delete=models.CASCADE,
+        to_field='workorder'
+    )
+    quantity = models.IntegerField()
+    comments = models.TextField()
+    created = models.DateTimeField()
+
+class rejects(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    employee = models.ForeignKey(
+        employee, 
+        on_delete=models.CASCADE
+    )
+    shift = models.CharField(
+        max_length=1
+    )
+    line = models.ForeignKey(
+        line, 
+        on_delete=models.CASCADE,
+        to_field='name'
+    )
+    date = models.DateField()
+    workorder = models.ForeignKey(
+        workorder,
+        on_delete=models.CASCADE,
+        to_field='workorder'
+    )
+    quantity = models.IntegerField()
+    reason = models.TextField()
+    created = models.DateTimeField()
+
+class downtime(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    line = models.ForeignKey(
+        line, 
+        on_delete=models.CASCADE,
+        to_field='name'
+    )
+    start_time = models.DateTimeField()
+    employee_start = models.ForeignKey(
+        employee, 
+        on_delete=models.CASCADE,
+        related_name='downtime_start_set'
+    )
+    end_time = models.DateTimeField()
+    reason = models.TextField()
+    employee_end = models.ForeignKey(
+        employee, 
+        on_delete=models.CASCADE,
+        related_name='downtime_end_set'
+    )
+
+    def __str__(self):
+        return f"Duration: {self.start_time - self.end_time}"
+    
