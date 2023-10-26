1dc6556d34420ddb9618f865da98f6f88c426e15
SRCthird <SRCthird@gmail.com>
2023-10-26 17:49:46 -0400

    Create and Update downtime entry form

---
+++ line_management/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class DowntimeCreateForm(forms.Form):
+    employee = forms.CharField()
+    line = forms.CharField()
+
+class DowntimeUpdateForm(forms.Form):
+    employee = forms.CharField()
+    reason = forms.CharField()
+    comments = forms.CharField()
