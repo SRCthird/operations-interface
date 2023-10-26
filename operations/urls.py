5e4d0a0fbe89a6690b0d22ec81ed2eae96ea25f7
SRCthird <SRCthird@gmail.com>
2023-10-26 17:50:11 -0400

    Add create and update downtime endpoint

--- operations/urls.py
+++ operations/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('material/add/', views.MaterialCreateView.as_view(), name='material_create'),
     path('material/<int:pk>/edit/', views.MaterialUpdateView.as_view(), name='material_update'),
     path('material/<int:pk>/delete/', views.MaterialDeleteView.as_view(), name='material_delete'),
-    path('line/<str:pk>/<str:shift>/', line_views.line_view, name='line_view')
+    path('line/<str:pk>/<str:shift>/', line_views.line_view, name='line_view'),
+    path('update_downtime/<int:downtime_id>/', line_views.update_downtime, name='update_downtime'),
+    path('create_downtime/', line_views.create_downtime, name='create_downtime'),
 ]
